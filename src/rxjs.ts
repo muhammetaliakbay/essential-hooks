73856cdcaf4aa56712fe734741278689c1e6b8bf
Muhammet Ali AKBAY <me@muhammetaliakbay.com>
2020-06-19 11:02:20 +0300

    RxJs useObservable fixes
    + Added overload signatures for useObservable hook in rxjs.ts

--- src/rxjs.ts
+++ src/rxjs.ts
@@ -1,7 +1,11 @@
 import {Observable} from 'rxjs';
 import {useEffect, useState} from 'react';
 
-export function useObservable<T>(observable: Observable<T>, initialValue?: T): T | typeof initialValue {
+export function useObservable<T>(observable: Observable<T>): T | undefined;
+export function useObservable<T>(observable: Observable<T>, initialValue: T): T;
+export function useObservable<T>(observable: Observable<T>, initialValue: undefined): T | undefined;
+
+export function useObservable<T>(observable: Observable<T>, initialValue?: T) {
     const [value, setValue] = useState(initialValue);
     useEffect(() => {
         const subscription = observable.subscribe(setValue);
